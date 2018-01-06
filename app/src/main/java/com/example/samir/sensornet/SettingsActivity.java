b19a37b37f1fde4d410231beb14d6287701ec710
dmb <daomalick225@gmail.com>
2018-01-06 17:00:54 +0100

    Settings Activity Toolbar upgraded

--- app/src/main/java/com/example/samir/sensornet/SettingsActivity.java
+++ app/src/main/java/com/example/samir/sensornet/SettingsActivity.java
@@ -1,17 +1,27 @@
 package com.example.samir.sensornet;
-
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SettingsActivity extends MenuActivity {
+
+    Toolbar settingsToolbar;
+    String [] items = {"Pression", "Temperature"};
+    ArrayAdapter<String> listAdapter;
+    private ArrayList<String> arrayList;
+    private GridView boundariesGridView;
 
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth fbAuth;
@@ -31,6 +41,17 @@ public class SettingsActivity extends MenuActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        settingsToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(settingsToolbar);
+
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        boundariesGridView = (GridView) findViewById(R.id.sensorBoundariesGrid);
+        boundariesGridView.setAdapter(listAdapter);
+
+        listAdapter.notifyDataSetChanged();
+
         //Getting Firebase auth instance
         fbAuth = FirebaseAuth.getInstance();
         signOut = (Button) findViewById(R.id.sign_out);
@@ -40,6 +61,25 @@ public class SettingsActivity extends MenuActivity {
                 signOut();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id == R.id.action_log_out){
+            signOut();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //sign out method
